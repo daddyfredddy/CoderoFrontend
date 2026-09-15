c84d0b59a1c6a8c735b43945532a80c2b21f4954
unknown <fredrickcodero@gmail.com>
2026-09-15 18:14:32 +0300

    second commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <img src="/assets/images/logo.png" alt="App logo" class="w-16 h-16" />
+      </div>
+      <div>
+        <h1>Home</h1>
+      </div>
+      <img />
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../Components/Navbar';
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1 class="font-fonty1  bg-kalas text-2xl text-green">Welcome to Codero</h1>
+    </div>
+  );
+}
+
+export default Home;
